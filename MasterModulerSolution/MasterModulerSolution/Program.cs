0e83fb961ad0b6f6d5cfb6e8667e5ddf285a5f8d
Simone Cosma <scosmaa@gmail.com>
2019-12-19 16:12:14 +0100

    First commit

---
+++ MasterModulerSolution/MasterModulerSolution/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterModulerSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I'm the master module!");
+            Console.ReadKey();
+        }
+    }
+}
