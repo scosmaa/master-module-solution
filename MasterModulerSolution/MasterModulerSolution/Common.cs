9a00712e267322fb1eb6dbeeb03b0ca037b07034
Simone Cosma <scosmaa@gmail.com>
2019-12-19 16:30:09 +0100

    Added Module

--- MasterModulerSolution/MasterModulerSolution/Common.cs
+++ MasterModulerSolution/MasterModulerSolution/Common.cs
@@ -6,11 +6,16 @@ using System.Threading.Tasks;
 
 namespace MasterModulerSolution
 {
-    class Common
+    public class Common
     {
         public static string Test()
         {
             return "Master Module class";
+        }
+
+        public static string Test2()
+        {
+            return "Master Module class - Test 2";
         }
     }
 }
